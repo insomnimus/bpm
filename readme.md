5b4880c2c25295d3e907fd8a3249b851fcf906e9
Taylan Gökkaya <insomnimus@proton.me>
2024-02-03 01:23:34 +0300

    initial commit

---
+++ readme.md
@@ -0,0 +1,10 @@
+# BPM
+`bpm` is a simple console app that lets you tap keyboard keys to measure beats per minute (BPM).
+
+## Installation
+You need a recent Rust toolchain installed.
+
+Run:
+```shell
+cargo install --git https://github.com/insomnimus/bpm --branch main
+```
---
+++ src/main.rs
@@ -0,0 +1,128 @@
+use std::{
+	env,
+	io::{
+		stdout,
+		Result,
+		Write,
+	},
+	process,
+	time::Instant,
+};
+
+use crossterm::{
+	event::{
+		self,
+		Event,
+		KeyCode,
+		KeyEventKind,
+		KeyModifiers,
+		KeyboardEnhancementFlags,
+		PopKeyboardEnhancementFlags,
+		PushKeyboardEnhancementFlags,
+	},
+	terminal,
+	ExecutableCommand,
+};
+
+const VERSION: &str = env!("CARGO_PKG_VERSION");
+const NAME: &str = env!("CARGO_PKG_NAME");
+
+fn show_help() {
+	println!(
+		"\
+{NAME} v{VERSION}
+Tap keys to calculate beats per minute (BPM)
+
+OPTIONS:
+  -h, --help: Print usage help
+  -V, --version: Print version information\
+"
+	);
+}
+
+fn run() -> Result<()> {
+	let mut enhanced = cfg!(windows) || terminal::supports_keyboard_enhancement().unwrap_or(false);
+	terminal::enable_raw_mode()?;
+	if !cfg!(windows) && enhanced {
+		enhanced = stdout()
+			.execute(PushKeyboardEnhancementFlags(
+				KeyboardEnhancementFlags::REPORT_EVENT_TYPES,
+			))
+			.is_ok();
+	}
+
+	let close = || {
+		if enhanced {
+			let _ = stdout().execute(PopKeyboardEnhancementFlags);
+		}
+		terminal::disable_raw_mode()
+	};
+
+	stdout().write_all(b"tap characters to start timing bpm\npress esc or ctrl+c to stop timing\nthe timing is from the first character pressed to the last one\n")?;
+
+	// Read once to start the timer.
+	loop {
+		if let Event::Key(k) = event::read()? {
+			if enhanced && k.kind != KeyEventKind::Release {
+				continue;
+			}
+
+			match k.code {
+				KeyCode::Char('c' | 'C') if k.modifiers == KeyModifiers::CONTROL => return close(),
+				KeyCode::Esc => return close(),
+				KeyCode::Char(_) if k.modifiers.is_empty() => break,
+				_ => (),
+			}
+		}
+	}
+
+	let start = Instant::now();
+	let mut last = start;
+	let mut count = 0_u32;
+
+	loop {
+		if let Event::Key(k) = event::read()? {
+			if enhanced && k.kind != KeyEventKind::Release {
+				continue;
+			}
+
+			match k.code {
+				KeyCode::Char('c' | 'C') if k.modifiers == KeyModifiers::CONTROL => break,
+				KeyCode::Esc => break,
+				KeyCode::Char(_) if k.modifiers.is_empty() => {
+					count += 1;
+					last = Instant::now();
+				}
+				_ => (),
+			}
+		}
+	}
+
+	close()?;
+	let ns = (last - start).as_nanos();
+	let minutes = ns as f64 / 60e9_f64;
+	let bpm = count as f64 / minutes;
+	println!("bpm: {bpm:.4}");
+	Ok(())
+}
+
+fn main() {
+	if let Some(a) = env::args_os().nth(1) {
+		let exit_code = if &a == "-h" || &a == "--help" {
+			show_help();
+			0
+		} else if &a == "-V" || &a == "--version" {
+			println!("{NAME} v{VERSION}");
+			0
+		} else {
+			eprintln!("error: unknown option {}", a.as_os_str().to_string_lossy());
+			1
+		};
+		process::exit(exit_code);
+	}
+
+	if let Err(e) = run() {
+		eprintln!("error: {e}");
+		process::exit(1);
+	}
+}
